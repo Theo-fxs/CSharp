dc9c096c30dca784f9181127a55000dbc3405f42
Ansuman <kazukikiryu6@gmail.com>
2025-01-30 16:28:49 +0000

    Add files via upload

---
+++ c#/Exercicio 16/Exercicio 16/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+internal class Exercicio_16
+{
+    public static void Main(string[] args)
+    {
+        int numero = 10;
+        bool resultado = valor(numero);
+        if (resultado)
+        {
+            int squareroot = numero * numero;
+            Console.WriteLine("Este numero e par e o quadrado e " + squareroot);   
+        }
+        else
+        {
+            Console.WriteLine("Este numero e impar");
+        }
+    }
+    public static bool valor(int a)
+    {
+        return a % 2 == 0;
+    } 
+
+}
---
+++ c#/Exercicio16Correcao/Exercicio16Correcao/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+
+public class Exercicio16Correcao
+{
+        public static void Main(string[] args)
+        {
+
+        parImpar(10);
+        parImpar(11);
+
+        }
+
+    public static void parImpar(int valor1)
+    {
+        bool resultado = valor1 % 2 == 0;
+
+        if (resultado)
+        {
+            quadradoNumPar(valor1);
+
+        }
+        else
+        {
+            Console.WriteLine("O numero nao e impar e por " +
+                "isso nao e calculado o valor e quadrado ");
+        }
+    }
+    public static void quadradoNumPar(int valor2)
+    {
+        int resultado = valor2 * valor2;
+        Console.WriteLine("O quadrado do valor " + valor2 + " e " + resultado);
+    }
+
+
+
+}
+
