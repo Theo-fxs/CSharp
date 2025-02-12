6e5608af2e4f2830c42bc526d1ce1201c2d2b15c
Ansuman <kazukikiryu6@gmail.com>
2025-02-12 10:32:58 +0000

    Add files via upload

---
+++ c#/Exercicio17/Exercicio17/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // string e para texto NAO PODE USAR INT :D
+
+        String [] ListaFilmes = {"Harry Potter" ,"Zorro" ,"Simpsons"};
+        int[] Ano = {1999 , 2010 , 2014 };
+        Console.WriteLine("Lista de Filmes: \n ");
+
+        // isso e uh o coiso que faz o loop :D 
+
+        for (int contador = 0; contador < ListaFilmes.Length; contador++)
+        {
+            Console.WriteLine(ListaFilmes[contador] + " - " + Ano[contador]);
+        }
+       
+
+    }
+}
---
+++ c#/Exercicio18/Exercicio18/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // string e para texto NAO PODE USAR INT :D
+
+        String[] cidades = { "Lisboa", "Porto", "Coimbra" ,"Faro","Braga"  };
+        int[] populacao = { 545000, 237000, 143000 , 118000 , 193000 };
+        Console.WriteLine("Cidades e as suas populacoes: \n ");
+
+        // isso e uh o coiso que faz o loop :D 
+
+        for (int contador = 0; contador < cidades.Length; contador++)
+        {
+            Console.WriteLine(cidades[contador] + " - " + populacao[contador] + " habitantes");
+        }
+
+
+    }
+}
