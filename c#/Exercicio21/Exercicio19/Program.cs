8f87310774be615fc1ba44ec696b5aaa87458382
Ansuman <kazukikiryu6@gmail.com>
2025-04-02 10:34:13 +0100

    Add files via upload

---
+++ c#/Exercicio21/Exercicio19/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+public class Exercicio19
+{   
+        public static void Main(string[] args)
+        {
+         List<string> listaNomes = new List<string>();
+         Console.WriteLine("Insira 5 nomes : ");
+        for(int contador = 0; contador < 5; contador++)
+        {
+            String nome = Console.ReadLine();
+            listaNomes.Add(nome);
+        }
+        Console.WriteLine("Lista de nomes: " );
+        foreach (string nome in listaNomes) {
+        Console.WriteLine(nome);
+        }
+        listaNomes.Sort();
+        Console.WriteLine("Lista de nomes organizado");
+        foreach (string nome in listaNomes) {
+        Console.WriteLine(nome);
+        }
+        
+        }   
+}
